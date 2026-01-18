fd80813232ada98696409c77e87ec118825e750a
ariiannemay <83134165+ariiannemay@users.noreply.github.com>
2026-01-19 05:52:11 +0800

    Create calendar-syllabus.js

---
+++ calendar-syllabus.js
@@ -0,0 +1,98 @@
+window.CALENDAR_SYLLABUS = {
+    poli: { 
+        title: 'POLITICAL AND PUBLIC INTERNATIONAL LAW', 
+        shortTitle: 'POLI LAW', 
+        color: '#795548', 
+        data: [
+            {id: 5001, topic: "I. BASIC CONCEPTS", depth: 1, trackable: true},
+            {id: 5007, topic: "II. NATIONAL TERRITORY", depth: 1, trackable: true},
+            {id: 5012, topic: "III. CITIZENSHIP", depth: 1, trackable: true},
+            {id: 5016, topic: "IV. LEGISLATIVE DEPARTMENT", depth: 1, trackable: true},
+            {id: 5022, topic: "V. EXECUTIVE DEPARTMENT", depth: 1, trackable: true},
+            {id: 5028, topic: "VI. JUDICIAL DEPARTMENT", depth: 1, trackable: true},
+            {id: 5033, topic: "VII. CONSTITUTIONAL COMMISSIONS", depth: 1, trackable: true},
+            {id: 5036, topic: "VIII. CONSTITUTIONAL RIGHTS", depth: 1, trackable: true},
+            {id: 5045, topic: "IX. NATIONAL ECONOMY AND PATRIMONY", depth: 1, trackable: true},
+            {id: 5048, topic: "X. ADMINISTRATIVE LAW", depth: 1, trackable: true},
+            {id: 5052, topic: "XI. LAW ON PUBLIC OFFICERS", depth: 1, trackable: true},
+            {id: 5056, topic: "XII. ELECTION LAW", depth: 1, trackable: true},
+            {id: 5060, topic: "XIII. LOCAL GOVERNMENTS", depth: 1, trackable: true},
+            {id: 5064, topic: "XIV. PUBLIC INTERNATIONAL LAW", depth: 1, trackable: true}
+        ]
+    },
+    comm: { 
+        title: 'COMMERCIAL AND TAXATION LAWS', 
+        shortTitle: 'COMM & TAX', 
+        color: '#eab308', 
+        data: [
+            {id: 4001, topic: 'I. BUSINESS ORGANIZATIONS', depth: 1, trackable: true},
+            {id: 4005, topic: 'II. INSURANCE', depth: 1, trackable: true},
+            {id: 4010, topic: 'III. TRANSPORTATION', depth: 1, trackable: true},
+            {id: 4014, topic: 'IV. BANKING', depth: 1, trackable: true},
+            {id: 4019, topic: 'V. INTELLECTUAL PROPERTY', depth: 1, trackable: true},
+            {id: 4023, topic: 'VI. SPECIAL COMMERCIAL LAWS', depth: 1, trackable: true},
+            {id: 4027, topic: 'VII. TAXATION LAW', depth: 1, trackable: true}
+        ]
+    },
+    civ: { 
+        title: 'CIVIL LAW AND LAND TITLES AND DEEDS', 
+        shortTitle: 'CIVIL LAW', 
+        color: '#1e3a8a', 
+        data: [
+            {id: 3001, topic: "I. EFFECT AND APPLICATION OF LAWS", depth: 1, trackable: true},
+            {id: 3005, topic: "II. PERSONS", depth: 1, trackable: true},
+            {id: 3011, topic: "III. FAMILY CODE", depth: 1, trackable: true},
+            {id: 3015, topic: "IV. CIVIL REGISTER", depth: 1, trackable: true},
+            {id: 3024, topic: "V. PROPERTY, OWNERSHIP, AND ITS MODIFICATIONS", depth: 1, trackable: true},
+            {id: 3030, topic: "VI. LAND TITLES AND DEEDS", depth: 1, trackable: true},
+            {id: 3035, topic: "VII. SUCCESSION", depth: 1, trackable: true},
+            {id: 3041, topic: "VIII. OBLIGATIONS AND CONTRACTS", depth: 1, trackable: true},
+            {id: 3046, topic: "IX. SPECIAL CONTRACTS", depth: 1, trackable: true},
+            {id: 3050, topic: "X. QUASI-CONTRACTS", depth: 1, trackable: true},
+            {id: 3053, topic: "XI. TORTS AND QUASI-DELICTS", depth: 1, trackable: true},
+            {id: 3060, topic: "XII. DAMAGES", depth: 1, trackable: true}
+        ]
+    },
+    lab: { 
+        title: 'LABOR AND SOCIAL LEGISLATION', 
+        shortTitle: 'LABOR LAW', 
+        color: '#38761d', 
+        data: [
+            {id: 2001, topic: "I. BASIC PRINCIPLES AND CONCEPTS", depth: 1, trackable: true},
+            {id: 2008, topic: "II. RECRUITMENT AND PLACEMENT", depth: 1, trackable: true},
+            {id: 2014, topic: "III. EMPLOYMENT RELATIONSHIP", depth: 1, trackable: true},
+            {id: 2019, topic: "IV. LABOR STANDARDS", depth: 1, trackable: true},
+            {id: 2025, topic: "V. LABOR RELATIONS", depth: 1, trackable: true},
+            {id: 2031, topic: "VI. SUSPENSION AND TERMINATION OF EMPLOYMENT", depth: 1, trackable: true},
+            {id: 2037, topic: "VII. SOCIAL LEGISLATION", depth: 1, trackable: true},
+            {id: 2042, topic: "VIII. LABOR ADJUDICATION-JURISDICTION & REMEDIES", depth: 1, trackable: true}
+        ]
+    },
+    crim: { 
+        title: 'CRIMINAL LAW', 
+        shortTitle: 'CRIM LAW', 
+        color: '#b33a3a', 
+        data: [
+            {id: 1001, topic: "I. FUNDAMENTAL PRINCIPLES", depth: 1, trackable: true},
+            {id: 1013, topic: "II. FELONIES AND CRIMINAL LIABILITY", depth: 1, trackable: true},
+            {id: 1032, topic: "III. CRIMES AND THEIR PENALTIES", depth: 1, trackable: true}
+        ]
+    },
+    rem: { 
+        title: 'REMEDIAL LAW, LEGAL ETHICS, & LEGAL FORMS', 
+        shortTitle: 'REM LAW', 
+        color: '#674ea7', 
+        data: [
+            {id: 1, topic: "I. GENERAL PRINCIPLES", depth: 1, trackable: true},
+            {id: 9, topic: "II. JURISDICTION", depth: 1, trackable: true},
+            {id: 43, topic: "III. CIVIL PROCEDURE", depth: 1, trackable: true},
+            {id: 158, topic: "IV. PROVISIONAL REMEDIES", depth: 1, trackable: true},
+            {id: 174, topic: "V. SPECIAL CIVIL ACTIONS", depth: 1, trackable: true},
+            {id: 190, topic: "VI. SPECIAL PROCEEDINGS AND WRITS", depth: 1, trackable: true},
+            {id: 217, topic: "VII. CRIMINAL PROCEDURE", depth: 1, trackable: true},
+            {id: 335, topic: "VIII. EVIDENCE", depth: 1, trackable: true},
+            {id: 414, topic: "IX. LEGAL AND JUDICIAL ETHICS", depth: 1, trackable: true},
+            {id: 583, topic: "X. PRACTICAL EXERCISES", depth: 1, trackable: true}
+        ]
+    }
+};
