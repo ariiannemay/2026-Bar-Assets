77ed2e4cea606451f5d4ae672e2de182e5b737bd
ariiannemay <83134165+ariiannemay@users.noreply.github.com>
2026-01-19 06:11:28 +0800

    Update calendar-syllabus.js

--- calendar-syllabus.js
+++ calendar-syllabus.js
@@ -4,20 +4,20 @@ window.CALENDAR_SYLLABUS = {
         shortTitle: 'POLI LAW', 
         color: '#795548', 
         data: [
-            {id: 5001, topic: "I. BASIC CONCEPTS", depth: 1, trackable: true},
-            {id: 5007, topic: "II. NATIONAL TERRITORY", depth: 1, trackable: true},
-            {id: 5012, topic: "III. CITIZENSHIP", depth: 1, trackable: true},
-            {id: 5016, topic: "IV. LEGISLATIVE DEPARTMENT", depth: 1, trackable: true},
-            {id: 5022, topic: "V. EXECUTIVE DEPARTMENT", depth: 1, trackable: true},
-            {id: 5028, topic: "VI. JUDICIAL DEPARTMENT", depth: 1, trackable: true},
-            {id: 5033, topic: "VII. CONSTITUTIONAL COMMISSIONS", depth: 1, trackable: true},
-            {id: 5036, topic: "VIII. CONSTITUTIONAL RIGHTS", depth: 1, trackable: true},
-            {id: 5045, topic: "IX. NATIONAL ECONOMY AND PATRIMONY", depth: 1, trackable: true},
-            {id: 5048, topic: "X. ADMINISTRATIVE LAW", depth: 1, trackable: true},
-            {id: 5052, topic: "XI. LAW ON PUBLIC OFFICERS", depth: 1, trackable: true},
-            {id: 5056, topic: "XII. ELECTION LAW", depth: 1, trackable: true},
-            {id: 5060, topic: "XIII. LOCAL GOVERNMENTS", depth: 1, trackable: true},
-            {id: 5064, topic: "XIV. PUBLIC INTERNATIONAL LAW", depth: 1, trackable: true}
+            {id: "POLI-CALENDAR-0001", topic: "I. BASIC CONCEPTS", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0002", topic: "II. NATIONAL TERRITORY", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0003", topic: "III. CITIZENSHIP", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0004", topic: "IV. LEGISLATIVE DEPARTMENT", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0005", topic: "V. EXECUTIVE DEPARTMENT", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0006", topic: "VI. JUDICIAL DEPARTMENT", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0007", topic: "VII. CONSTITUTIONAL COMMISSIONS", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0008", topic: "VIII. CONSTITUTIONAL RIGHTS", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0009", topic: "IX. NATIONAL ECONOMY AND PATRIMONY", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0010", topic: "X. ADMINISTRATIVE LAW", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0011", topic: "XI. LAW ON PUBLIC OFFICERS", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0012", topic: "XII. ELECTION LAW", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0013", topic: "XIII. LOCAL GOVERNMENTS", depth: 1, trackable: true},
+            {id: "POLI-CALENDAR-0014", topic: "XIV. PUBLIC INTERNATIONAL LAW", depth: 1, trackable: true}
         ]
     },
     comm: { 
@@ -25,13 +25,13 @@ window.CALENDAR_SYLLABUS = {
         shortTitle: 'COMM & TAX', 
         color: '#eab308', 
         data: [
-            {id: 4001, topic: 'I. BUSINESS ORGANIZATIONS', depth: 1, trackable: true},
-            {id: 4005, topic: 'II. INSURANCE', depth: 1, trackable: true},
-            {id: 4010, topic: 'III. TRANSPORTATION', depth: 1, trackable: true},
-            {id: 4014, topic: 'IV. BANKING', depth: 1, trackable: true},
-            {id: 4019, topic: 'V. INTELLECTUAL PROPERTY', depth: 1, trackable: true},
-            {id: 4023, topic: 'VI. SPECIAL COMMERCIAL LAWS', depth: 1, trackable: true},
-            {id: 4027, topic: 'VII. TAXATION LAW', depth: 1, trackable: true}
+            {id: "COMM-CALENDAR-0001", topic: 'I. BUSINESS ORGANIZATIONS', depth: 1, trackable: true},
+            {id: "COMM-CALENDAR-0002", topic: 'II. INSURANCE', depth: 1, trackable: true},
+            {id: "COMM-CALENDAR-0003", topic: 'III. TRANSPORTATION', depth: 1, trackable: true},
+            {id: "COMM-CALENDAR-0004", topic: 'IV. BANKING', depth: 1, trackable: true},
+            {id: "COMM-CALENDAR-0005", topic: 'V. INTELLECTUAL PROPERTY', depth: 1, trackable: true},
+            {id: "COMM-CALENDAR-0006", topic: 'VI. SPECIAL COMMERCIAL LAWS', depth: 1, trackable: true},
+            {id: "COMM-CALENDAR-0007", topic: 'VII. TAXATION LAW', depth: 1, trackable: true}
         ]
     },
     civ: { 
@@ -39,18 +39,18 @@ window.CALENDAR_SYLLABUS = {
         shortTitle: 'CIVIL LAW', 
         color: '#1e3a8a', 
         data: [
-            {id: 3001, topic: "I. EFFECT AND APPLICATION OF LAWS", depth: 1, trackable: true},
-            {id: 3005, topic: "II. PERSONS", depth: 1, trackable: true},
-            {id: 3011, topic: "III. FAMILY CODE", depth: 1, trackable: true},
-            {id: 3015, topic: "IV. CIVIL REGISTER", depth: 1, trackable: true},
-            {id: 3024, topic: "V. PROPERTY, OWNERSHIP, AND ITS MODIFICATIONS", depth: 1, trackable: true},
-            {id: 3030, topic: "VI. LAND TITLES AND DEEDS", depth: 1, trackable: true},
-            {id: 3035, topic: "VII. SUCCESSION", depth: 1, trackable: true},
-            {id: 3041, topic: "VIII. OBLIGATIONS AND CONTRACTS", depth: 1, trackable: true},
-            {id: 3046, topic: "IX. SPECIAL CONTRACTS", depth: 1, trackable: true},
-            {id: 3050, topic: "X. QUASI-CONTRACTS", depth: 1, trackable: true},
-            {id: 3053, topic: "XI. TORTS AND QUASI-DELICTS", depth: 1, trackable: true},
-            {id: 3060, topic: "XII. DAMAGES", depth: 1, trackable: true}
+            {id: "CIV-CALENDAR-0001", topic: "I. EFFECT AND APPLICATION OF LAWS", depth: 1, trackable: true},
+            {id: "CIV-CALENDAR-0002", topic: "II. PERSONS", depth: 1, trackable: true},
+            {id: "CIV-CALENDAR-0003", topic: "III. FAMILY CODE", depth: 1, trackable: true},
+            {id: "CIV-CALENDAR-0004", topic: "IV. CIVIL REGISTER", depth: 1, trackable: true},
+            {id: "CIV-CALENDAR-0005", topic: "V. PROPERTY, OWNERSHIP, AND ITS MODIFICATIONS", depth: 1, trackable: true},
+            {id: "CIV-CALENDAR-0006", topic: "VI. LAND TITLES AND DEEDS", depth: 1, trackable: true},
+            {id: "CIV-CALENDAR-0007", topic: "VII. SUCCESSION", depth: 1, trackable: true},
+            {id: "CIV-CALENDAR-0008", topic: "VIII. OBLIGATIONS AND CONTRACTS", depth: 1, trackable: true},
+            {id: "CIV-CALENDAR-0009", topic: "IX. SPECIAL CONTRACTS", depth: 1, trackable: true},
+            {id: "CIV-CALENDAR-0010", topic: "X. QUASI-CONTRACTS", depth: 1, trackable: true},
+            {id: "CIV-CALENDAR-0011", topic: "XI. TORTS AND QUASI-DELICTS", depth: 1, trackable: true},
+            {id: "CIV-CALENDAR-0012", topic: "XII. DAMAGES", depth: 1, trackable: true}
         ]
     },
     lab: { 
@@ -58,14 +58,14 @@ window.CALENDAR_SYLLABUS = {
         shortTitle: 'LABOR LAW', 
         color: '#38761d', 
         data: [
-            {id: 2001, topic: "I. BASIC PRINCIPLES AND CONCEPTS", depth: 1, trackable: true},
-            {id: 2008, topic: "II. RECRUITMENT AND PLACEMENT", depth: 1, trackable: true},
-            {id: 2014, topic: "III. EMPLOYMENT RELATIONSHIP", depth: 1, trackable: true},
-            {id: 2019, topic: "IV. LABOR STANDARDS", depth: 1, trackable: true},
-            {id: 2025, topic: "V. LABOR RELATIONS", depth: 1, trackable: true},
-            {id: 2031, topic: "VI. SUSPENSION AND TERMINATION OF EMPLOYMENT", depth: 1, trackable: true},
-            {id: 2037, topic: "VII. SOCIAL LEGISLATION", depth: 1, trackable: true},
-            {id: 2042, topic: "VIII. LABOR ADJUDICATION-JURISDICTION & REMEDIES", depth: 1, trackable: true}
+            {id: "LAB-CALENDAR-0001", topic: "I. BASIC PRINCIPLES AND CONCEPTS", depth: 1, trackable: true},
+            {id: "LAB-CALENDAR-0002", topic: "II. RECRUITMENT AND PLACEMENT", depth: 1, trackable: true},
+            {id: "LAB-CALENDAR-0003", topic: "III. EMPLOYMENT RELATIONSHIP", depth: 1, trackable: true},
+            {id: "LAB-CALENDAR-0004", topic: "IV. LABOR STANDARDS", depth: 1, trackable: true},
+            {id: "LAB-CALENDAR-0005", topic: "V. LABOR RELATIONS", depth: 1, trackable: true},
+            {id: "LAB-CALENDAR-0006", topic: "VI. SUSPENSION AND TERMINATION OF EMPLOYMENT", depth: 1, trackable: true},
+            {id: "LAB-CALENDAR-0007", topic: "VII. SOCIAL LEGISLATION", depth: 1, trackable: true},
+            {id: "LAB-CALENDAR-0008", topic: "VIII. LABOR ADJUDICATION-JURISDICTION & REMEDIES", depth: 1, trackable: true}
         ]
     },
     crim: { 
@@ -73,9 +73,9 @@ window.CALENDAR_SYLLABUS = {
         shortTitle: 'CRIM LAW', 
         color: '#b33a3a', 
         data: [
-            {id: 1001, topic: "I. FUNDAMENTAL PRINCIPLES", depth: 1, trackable: true},
-            {id: 1013, topic: "II. FELONIES AND CRIMINAL LIABILITY", depth: 1, trackable: true},
-            {id: 1032, topic: "III. CRIMES AND THEIR PENALTIES", depth: 1, trackable: true}
+            {id: "CRIM-CALENDAR-0001", topic: "I. FUNDAMENTAL PRINCIPLES", depth: 1, trackable: true},
+            {id: "CRIM-CALENDAR-0002", topic: "II. FELONIES AND CRIMINAL LIABILITY", depth: 1, trackable: true},
+            {id: "CRIM-CALENDAR-0003", topic: "III. CRIMES AND THEIR PENALTIES", depth: 1, trackable: true}
         ]
     },
     rem: { 
@@ -83,16 +83,16 @@ window.CALENDAR_SYLLABUS = {
         shortTitle: 'REM LAW', 
         color: '#674ea7', 
         data: [
-            {id: 1, topic: "I. GENERAL PRINCIPLES", depth: 1, trackable: true},
-            {id: 9, topic: "II. JURISDICTION", depth: 1, trackable: true},
-            {id: 43, topic: "III. CIVIL PROCEDURE", depth: 1, trackable: true},
-            {id: 158, topic: "IV. PROVISIONAL REMEDIES", depth: 1, trackable: true},
-            {id: 174, topic: "V. SPECIAL CIVIL ACTIONS", depth: 1, trackable: true},
-            {id: 190, topic: "VI. SPECIAL PROCEEDINGS AND WRITS", depth: 1, trackable: true},
-            {id: 217, topic: "VII. CRIMINAL PROCEDURE", depth: 1, trackable: true},
-            {id: 335, topic: "VIII. EVIDENCE", depth: 1, trackable: true},
-            {id: 414, topic: "IX. LEGAL AND JUDICIAL ETHICS", depth: 1, trackable: true},
-            {id: 583, topic: "X. PRACTICAL EXERCISES", depth: 1, trackable: true}
+            {id: "REM-CALENDAR-0001", topic: "I. GENERAL PRINCIPLES", depth: 1, trackable: true},
+            {id: "REM-CALENDAR-0002", topic: "II. JURISDICTION", depth: 1, trackable: true},
+            {id: "REM-CALENDAR-0003", topic: "III. CIVIL PROCEDURE", depth: 1, trackable: true},
+            {id: "REM-CALENDAR-0004", topic: "IV. PROVISIONAL REMEDIES", depth: 1, trackable: true},
+            {id: "REM-CALENDAR-0005", topic: "V. SPECIAL CIVIL ACTIONS", depth: 1, trackable: true},
+            {id: "REM-CALENDAR-0006", topic: "VI. SPECIAL PROCEEDINGS AND WRITS", depth: 1, trackable: true},
+            {id: "REM-CALENDAR-0007", topic: "VII. CRIMINAL PROCEDURE", depth: 1, trackable: true},
+            {id: "REM-CALENDAR-0008", topic: "VIII. EVIDENCE", depth: 1, trackable: true},
+            {id: "REM-CALENDAR-0009", topic: "IX. LEGAL AND JUDICIAL ETHICS", depth: 1, trackable: true},
+            {id: "REM-CALENDAR-0010", topic: "X. PRACTICAL EXERCISES", depth: 1, trackable: true}
         ]
     }
 };
